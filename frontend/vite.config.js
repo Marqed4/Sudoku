442f8c98b72c595489cef75c7eab2243641443f3
Marqed <Derpexsplosion@gmail.com>
2026-06-13 00:49:57 -0400

    Networking update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
+    preview: {
+        allowedHosts: ['sudoku.marqed.it', 'sudoku-production-7f4b.up.railway.app'],
+    },
     server: {
         port: 2001,
         proxy: {
